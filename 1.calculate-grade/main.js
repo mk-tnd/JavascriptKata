ec4a4bb3054ba7da18d4b91fe8d7bbc922021f1d
fResult <styxmaz@gmail.com>
2021-02-08 14:03:48 +0700

    Updated gitignore file

--- 1.calculate-grade/main.js
+++ 1.calculate-grade/main.js
@@ -1,17 +1,7 @@
 /**
- * @param {number} score
+ * @param { number } score
  * @returns { string }
  */
-function calculateGrade(score) {
-  if (score >= 76) {
-    return 'Hournor'
-  } else if (score >= 60) {
-    return 'Satisfactory'
-  } else {
-    return 'Unsatisfactory'
-  }
+exports.calculateGrade = (score) => {
+
 }
-
-
-
-module.exports = calculateGrade
