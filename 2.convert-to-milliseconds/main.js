3b4c3c4aa90a923e7a9d490808789e2f47e8d2bf
fResult <styxmaz@gmail.com>
2021-02-08 14:21:45 +0700

    Added Kata - Convert to milliseconds

---
+++ 2.convert-to-milliseconds/main.js
@@ -0,0 +1,21 @@
+/**
+ * @param { number } hours
+ * @param { number }  minutes
+ * @param { number }  seconds
+ * @return { number }
+ */
+function convertToMilliseconds(
+  hours,
+  minutes,
+  seconds
+) {
+  const millisecsPerSec = 1000
+  const sMillisecs = seconds * millisecsPerSec
+  const mMillisecs = minutes * 60 * millisecsPerSec
+  const hMillisecs = hours * 60 * 60 * millisecsPerSec
+  return sMillisecs + mMillisecs + hMillisecs
+}
+
+
+
+module.exports = convertToMilliseconds
---
+++ 2.convert-to-milliseconds/main.test.js
@@ -0,0 +1,22 @@
+const convertToMilliseconds = require('./main')
+
+describe('Convert to milliseconds', function() {
+  it(`should return 61000 when input 0, 0 and 0`, function() {
+    expect(convertToMilliseconds(0, 0, 0)).toBe(0)
+  })
+  it(`should return 61000 when input 0, 1 and 1`, function() {
+    expect(convertToMilliseconds(0, 1, 1)).toBe(61000)
+  })
+
+  it(`should return 3601000 when input 1, 0 and 1`, function() {
+    expect(convertToMilliseconds(1, 0, 1)).toBe(3601000)
+  })
+
+  it(`should return 3601000 when input 1, 1 and 1`, function() {
+    expect(convertToMilliseconds(1, 1, 1)).toBe(3661000)
+  })
+
+  it(`should return 13559000 when input 3, 45 and 59`, function() {
+    expect(convertToMilliseconds(3, 45, 59)).toBe(13559000)
+  })
+})
