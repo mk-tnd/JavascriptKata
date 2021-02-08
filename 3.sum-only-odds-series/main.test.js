907496c50011388a79e3e02e3f7181453cfdcf34
fResult <styxmaz@gmail.com>
2021-02-08 14:43:25 +0700

    Updated test file for clause 3

--- 3.sum-only-odds-series/main.test.js
+++ 3.sum-only-odds-series/main.test.js
@@ -1,19 +1,19 @@
 const sumOnlyOdds = require('./main')
 
 describe('Sum only odds from serial numbers', function() {
-  it('should return 25 when input 10 (1 + 2 + 3 + ... + 10)', function() {
+  it('should return 25 when input 10 (2 + 4 + 6 + ... + 10 = 25)', function() {
     expect(sumOnlyOdds(10)).toBe(25)
   })
-  it('should return 36 when input 12 (1 + 2 + 3 + ... + 12)', function() {
+  it('should return 36 when input 12 (2 + 4 + 6 + ... + 12 = 36)', function() {
     expect(sumOnlyOdds(12)).toBe(36)
   })
-  it('should return 625 when input 50 (1 + 2 + 3 + ... + 50)', function() {
-    expect(sumOnlyOdds(100)).toBe(2500)
+  it('should return 625 when input 50 (2 + 4 + 6 + ... + 50 = 625)', function() {
+    expect(sumOnlyOdds(100)).toBe(2_500)
   })
-  it('should return 2500 when input 100 (1 + 2 + 3 + ... + 100)', function() {
-    expect(sumOnlyOdds(100)).toBe(2500)
+  it('should return 2500 when input 100 (2 + 4 + 6 + ... + 100 = 2500)', function() {
+    expect(sumOnlyOdds(100)).toBe(2_500)
   })
-  it('should return 250000 when input 1000 (1 + 2 + 3 + ... + 1000)', function() {
-    expect(sumOnlyOdds(1000)).toBe(250000)
+  it('should return 250000 when input 1000 (2 + 4 + 6 + ... + 1000 = 25000)', function() {
+    expect(sumOnlyOdds(1000)).toBe(250_000)
   })
 })
