53a48b8b3268badac3ad393cb014b519943ee39d
fResult <styxmaz@gmail.com>
2021-02-10 18:55:29 +0700

    Updated test cases for clause 11

---
+++ 02.convert-to-milliseconds/main.js
@@ -0,0 +1,11 @@
+/**
+ * @param { number } hours
+ * @param { number }  minutes
+ * @param { number }  seconds
+ * @return { number }
+ */
+function convertToMilliseconds(hours, minutes, seconds) {
+
+}
+
+module.exports = convertToMilliseconds
--- 11.fizzbuzz/main.test.js
+++ 11.fizzbuzz/main.test.js
@@ -2,33 +2,40 @@ const { describe, it, expect } = require('@jest/globals')
 const fizzBuzz = require('./main')
 
 describe('fizzbuzz', function() {
-  it(`should return 'fizz' when input 3`, function(){
+  it(`should return 'fizz' when input 3`, function() {
     expect(fizzBuzz(3)).toBe('fizz')
   })
-  it(`should return 'buzz' when input 5`, function(){
+  it(`should return 'buzz' when input 5`, function() {
     expect(fizzBuzz(5)).toBe('buzz')
   })
-  it(`should return 'fizzbuzz' when input 15`, function(){
+  it(`should return 'fizzbuzz' when input 15`, function() {
     expect(fizzBuzz(15)).toBe('fizzbuzz')
   })
-  it(`should return 7 when input 7`, function(){
+  it(`should return 7 when input 7`, function() {
     expect(fizzBuzz(7)).toBe(7)
   })
 
-  it(`should return 'fizz' when input 18`, function(){
+  it(`should return 'fizz' when input 18`, function() {
     expect(fizzBuzz(3)).toBe('fizz')
   })
-  it(`should return 'buzz' when input 20`, function(){
+  it(`should return 'buzz' when input 20`, function() {
     expect(fizzBuzz(20)).toBe('buzz')
   })
-  it(`should return 'fizzbuzz' when input 45`, function(){
+  it(`should return 'fizzbuzz' when input 45`, function() {
     expect(fizzBuzz(45)).toBe('fizzbuzz')
   })
-  it(`should return 11 when input 11`, function(){
+  it(`should return 11 when input 11`, function() {
     expect(fizzBuzz(11)).toBe(11)
   })
 
-  it(`should return 'fizzbuzz' when input 300`, function(){
+  it(`should return 'fizzbuzz' when input 300`, function() {
     expect(fizzBuzz(300)).toBe('fizzbuzz')
+  })
+  it(`should return 'fizzbuzz' when input -315151515`, function() {
+    expect(fizzBuzz(-315151515)).toBe('fizzbuzz')
+  })
+
+  it(`should return 'fizzbuzz' when input -4564447`, function() {
+    expect(fizzBuzz(-4564447)).toBe(-4564447)
   })
 })
