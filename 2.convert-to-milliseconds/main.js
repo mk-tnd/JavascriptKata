c91bdbee71cc656a05e5084766278d1be783628e
fResult <styxmaz@gmail.com>
2021-02-10 16:06:24 +0700

    Added test case for Clause 2

--- 2.convert-to-milliseconds/main.js
+++ 2.convert-to-milliseconds/main.js
@@ -4,8 +4,17 @@
  * @param { number }  seconds
  * @return { number }
  */
-function convertToMilliseconds(hours, minutes, seconds) {
-
+function convertToMilliseconds(
+  hours,
+  minutes,
+  seconds
+) {
+  const millisecsPerSec = 1000
+  const sMillisecs = seconds * millisecsPerSec
+  const mMillisecs = minutes * 60 * millisecsPerSec
+  const hMillisecs = hours * 60 * 60 * millisecsPerSec
+  return sMillisecs + mMillisecs + hMillisecs
 }
+
 
 module.exports = convertToMilliseconds
--- 2.convert-to-milliseconds/main.test.js
+++ 2.convert-to-milliseconds/main.test.js
@@ -2,8 +2,17 @@ const { describe, it, expect } = require('@jest/globals')
 const convertToMilliseconds = require('./main')
 
 describe('Convert to milliseconds', function() {
-  it(`should return 61000 when input 0, 0 and 0`, function() {
+  it(`should return 0 when input 0, 0 and 0`, function() {
     expect(convertToMilliseconds(0, 0, 0)).toBe(0)
+  })
+  it(`should return 6000 when input 0, 0 and 1`, function() {
+    expect(convertToMilliseconds(0, 0, 1)).toBe(1000)
+  })
+  it(`should return 60000 when input 0, 1 and 0`, function() {
+    expect(convertToMilliseconds(0, 1, 0)).toBe(60000)
+  })
+  it(`should return 3600000 when input 1, 0 and 0`, function() {
+    expect(convertToMilliseconds(1, 0, 0)).toBe(3600000)
   })
   it(`should return 61000 when input 0, 1 and 1`, function() {
     expect(convertToMilliseconds(0, 1, 1)).toBe(61000)
