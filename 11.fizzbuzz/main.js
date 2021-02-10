5a32acbecffc2d952931f718ddf3814115afea63
fResult <styxmaz@gmail.com>
2021-02-10 18:03:10 +0700

    Added Clause 11 - FizzBuzz

---
+++ 11.fizzbuzz/main.js
@@ -0,0 +1,9 @@
+/**
+ * @param { number } num
+ * @return { 'fizz' | 'buzz' | 'fizzbuzz' | number }
+ */
+function fizzBuzz(num) {
+
+}
+
+module.exports = fizzBuzz
---
+++ 11.fizzbuzz/main.test.js
@@ -0,0 +1,34 @@
+const { describe, it, expect } = require('@jest/globals')
+const fizzBuzz = require('./main')
+
+describe('fizzbuzz', function() {
+  it(`should return 'fizz' when input 3`, function(){
+    expect(fizzBuzz(3)).toBe('fizz')
+  })
+  it(`should return 'buzz' when input 5`, function(){
+    expect(fizzBuzz(5)).toBe('buzz')
+  })
+  it(`should return 'fizzbuzz' when input 15`, function(){
+    expect(fizzBuzz(15)).toBe('fizzbuzz')
+  })
+  it(`should return 7 when input 7`, function(){
+    expect(fizzBuzz(7)).toBe(7)
+  })
+
+  it(`should return 'fizz' when input 18`, function(){
+    expect(fizzBuzz(3)).toBe('fizz')
+  })
+  it(`should return 'buzz' when input 20`, function(){
+    expect(fizzBuzz(20)).toBe('buzz')
+  })
+  it(`should return 'fizzbuzz' when input 45`, function(){
+    expect(fizzBuzz(45)).toBe('fizzbuzz')
+  })
+  it(`should return 11 when input 11`, function(){
+    expect(fizzBuzz(11)).toBe(11)
+  })
+
+  it(`should return 'fizzbuzz' when input 300`, function(){
+    expect(fizzBuzz(300)).toBe('fizzbuzz')
+  })
+})
