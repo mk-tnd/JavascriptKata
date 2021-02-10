dc649718cbdffc322e9b7185ba3a98cd6e2f740a
fResult <styxmaz@gmail.com>
2021-02-10 13:00:06 +0700

    Added Clause 10 - Array plus array

---
+++ 10.array-plus-array/main.js
@@ -0,0 +1,41 @@
+/**
+ * @param { Array<number> } arr1
+ * @param { Array<number> } arr2
+ * @return { Array<number> }
+ */
+function plusArray1(arr1, arr2) {
+  // Implement by FOR LOOP
+
+}
+
+/**
+ * @param { Array<number> } arr1
+ * @param { Array<number> } arr2
+ * @return { Array<number> }
+ */
+function plusArray2(arr1, arr2) {
+  // Implement by WHILE LOOP
+
+}
+
+/**
+ * @param { Array<number> } arr1
+ * @param { Array<number> } arr2
+ * @return { Array<number> }
+ */
+function plusArray3(arr1, arr2) {
+  // Implement by Array.prototype.map()
+
+}
+
+/**
+ * @param { Array<number> } arr1
+ * @param { Array<number> } arr2
+ * @return { Array<number> }
+ */
+function plusArray4(arr1, arr2) {
+  // Implement by Array.prototype.reduce()
+
+}
+
+module.exports = { plusArray1, plusArray2, plusArray3, plusArray4 }
---
+++ 10.array-plus-array/main.test.js
@@ -0,0 +1,49 @@
+const { plusArray1, plusArray2, plusArray3, plusArray4 } = require('./main')
+
+describe('Array plus array (For loop)', function() {
+  it('When input [1, 2, 3] and [4, 8, 5] should return [6, 10, 7]', () => {
+    expect(plusArray1([1, 2, 3], [4, 8, 5])).toEqual([6, 10, 7])
+  })
+  it('When input [1, 2, 3, 4] and [5, 6, 7, 8] should return [9, 9, 9, 9]', () => {
+    expect(plusArray1([1, 2, 3, 4], [5, 6, 7, 8])).toEqual([9, 9, 9, 9])
+  })
+  it('When input [5, 5, 5, 5], [4, 3, 2, 1] should return [9, 8, 7, 6]', () => {
+    expect(plusArray1([5, 5, 5, 5], [4, 3, 2, 1])).toEqual([6, 7, 8, 9])
+  })
+})
+
+describe('Array plus array (While loop)', function() {
+  it('When input [1, 2, 3] and [4, 8, 5] should return [6, 10, 7]', () => {
+    expect(plusArray2([1, 2, 3], [4, 8, 5])).toEqual([6, 10, 7])
+  })
+  it('When input [1, 2, 3, 4] and [5, 6, 7, 8] should return [9, 9, 9, 9]', () => {
+    expect(plusArray2([1, 2, 3, 4], [5, 6, 7, 8])).toEqual([9, 9, 9, 9])
+  })
+  it('When input [5, 5, 5, 5], [4, 3, 2, 1] should return [9, 8, 7, 6]', () => {
+    expect(plusArray2([5, 5, 5, 5], [4, 3, 2, 1])).toEqual([6, 7, 8, 9])
+  })
+})
+
+describe('Array plus array (Array.prototype.map())', function() {
+  it('When input [1, 2, 3] and [4, 8, 5] should return [6, 10, 7]', () => {
+    expect(plusArray3([1, 2, 3], [4, 8, 5])).toEqual([6, 10, 7])
+  })
+  it('When input [1, 2, 3, 4] and [5, 6, 7, 8] should return [9, 9, 9, 9]', () => {
+    expect(plusArray3([1, 2, 3, 4], [5, 6, 7, 8])).toEqual([9, 9, 9, 9])
+  })
+  it('When input [5, 5, 5, 5], [4, 3, 2, 1] should return [9, 8, 7, 6]', () => {
+    expect(plusArray3([5, 5, 5, 5], [4, 3, 2, 1])).toEqual([6, 7, 8, 9])
+  })
+})
+
+describe('Array plus array (Array.prototype.reduce())', function() {
+  it('When input [1, 2, 3] and [4, 8, 5] should return [6, 10, 7]', () => {
+    expect(plusArray4([1, 2, 3], [4, 8, 5])).toEqual([6, 10, 7])
+  })
+  it('When input [1, 2, 3, 4] and [5, 6, 7, 8] should return [9, 9, 9, 9]', () => {
+    expect(plusArray4([1, 2, 3, 4], [5, 6, 7, 8])).toEqual([9, 9, 9, 9])
+  })
+  it('When input [5, 5, 5, 5], [4, 3, 2, 1] should return [9, 8, 7, 6]', () => {
+    expect(plusArray4([5, 5, 5, 5], [4, 3, 2, 1])).toEqual([6, 7, 8, 9])
+  })
+})
