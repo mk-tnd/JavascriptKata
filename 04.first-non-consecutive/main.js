f522604d7326554f66b5a2825cd55258eb93ab06
mk-tnd <mickey_saejia@hotmail.com>
2021-02-12 13:59:47 +0700

    add Homework04

--- 04.first-non-consecutive/main.js
+++ 04.first-non-consecutive/main.js
@@ -3,7 +3,14 @@
  * @return { number | null }
  */
 function findFirstNonConsecutive(nums) {
-
+  if (nums.length > 1) {
+    for (let i = 0; i <= nums.length; i++) {
+      if (nums[i + 1] - nums[i] !== 1) {
+        return nums[i + 1]
+      }
+    }
+  }
+  return null
 }
 
 module.exports = findFirstNonConsecutive
