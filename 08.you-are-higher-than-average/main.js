2af1aad9de0b6938b469f6700e10693e62c8f32f
mk-tnd <mickey_saejia@hotmail.com>
2021-02-16 14:22:42 +0700

    update jskata

--- 08.you-are-higher-than-average/main.js
+++ 08.you-are-higher-than-average/main.js
@@ -4,7 +4,14 @@
  * @return { boolean }
  */
 function isHigherThanAverage(classScores, yourScore) {
+  let total = 0
+  for (let i = 0; i < classScores.length; i++) {
+    total = classScores[i] + total
+  }
+  const avg = total / classScores.length
+  let boolean = yourScore > avg ? true : false
 
+  return boolean
 }
 
 module.exports = isHigherThanAverage
--- 09.min-max-and-average/main.js
+++ 09.min-max-and-average/main.js
@@ -3,7 +3,16 @@
  * @return { {min: number, max: number, avg: number } }
  */
 function findMinMaxAndAvg(numbers) {
+  const max = Math.max(...numbers)
+  const min = Math.min(...numbers)
+  let result = 0
+  let avg
+  numbers.forEach((value) => {
+    result += value
+  })
+  avg = Math.round(result / numbers.length)
 
+  return { max, min, avg }
 }
 
 module.exports = findMinMaxAndAvg
