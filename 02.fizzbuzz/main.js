817d230936c24945afb1fe309a3790d06dba3b2e
mk-tnd <mickey_saejia@hotmail.com>
2021-02-23 16:47:34 +0700

    update 02

--- 02.fizzbuzz/main.js
+++ 02.fizzbuzz/main.js
@@ -3,7 +3,15 @@
  * @return { 'fizz' | 'buzz' | 'fizzbuzz' | number }
  */
 function fizzBuzz(num) {
-
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'fizzbuzz'
+  } else if (num % 5 === 0) {
+    return 'buzz'
+  } else if (num % 3 === 0) {
+    return 'fizz'
+  } else {
+    return num
+  }
 }
 
 module.exports = fizzBuzz
--- 10.longest-word/main.js
+++ 10.longest-word/main.js
@@ -3,7 +3,13 @@
  * @return { number }
  */
 function findLongestWord(phrase) {
-
+  const newword = phrase.split(/[\s\,\.\?\!]/)
+  let lenarr = []
+  for (let i = 0; i < newword.length; i++) {
+    lenarr[i] = newword[i].length
+  }
+  const result = Math.max(...lenarr)
+  return result
 }
 
 module.exports = findLongestWord
